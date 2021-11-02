9cfc4fbc16fda6de0d6be6a9175b4743c49af3d7
Sean Lee <lee.sw.sean@gmail.com>
2021-11-01 19:43:44 -0700

    update package.json

--- src/extension.ts
+++ src/extension.ts
@@ -4,8 +4,7 @@ import * as vscode from 'vscode';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
-	
+export const activate = (context: vscode.ExtensionContext): void => {
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "mountainvue" is now active!');
@@ -16,11 +15,11 @@ export function activate(context: vscode.ExtensionContext) {
 	let disposable = vscode.commands.registerCommand('mountainvue.helloWorld', () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from MountainVue!');
+		vscode.window.showInformationMessage('REBRAND');
 	});
 
 	context.subscriptions.push(disposable);
-}
+};
 
 // this method is called when your extension is deactivated
 export function deactivate() {}
