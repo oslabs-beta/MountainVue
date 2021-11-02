4e584c6833c533a59b5bcf8363c2edb73f3943e2
ryanlim1 <ryanlim301@gmail.com>
2021-11-02 11:36:59 -0700

    some updates but sidebar not working yet

--- src/extension.ts
+++ src/extension.ts
@@ -1,22 +1,35 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { SidebarProvider } from './SidebarProvider';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export const activate = (context: vscode.ExtensionContext): void => {
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "mountainvue" is now active!');
+	//console.log('Congratulations, your extension "mountainvue" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
+		const sidebarProvider = new SidebarProvider(context.extensionUri);
+  	context.subscriptions.push(
+    vscode.window.registerWebviewViewProvider(
+      "MV-sidebar",
+      sidebarProvider
+    )
+  );
+
 	let disposable = vscode.commands.registerCommand('mountainvue.helloWorld', () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
 		vscode.window.showInformationMessage('REBRAND');
 	});
+
+
+
+
+	// The command has been defined in the package.json file
+	// Now provide the implementation of the command with registerCommand
+	// The commandId parameter must match the command field in package.json
 
 	context.subscriptions.push(disposable);
 };
