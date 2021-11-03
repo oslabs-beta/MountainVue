bfe58eb732598cd251c10c96feb02a0fbefffb86
soheelee1985@gmail.com <soheelee1985@gmail.com>
2021-11-03 14:29:21 -0700

    sidebar and Icon added

--- src/extension.ts
+++ src/extension.ts
@@ -3,8 +3,7 @@
 import * as vscode from 'vscode';
 import { SidebarProvider } from './SidebarProvider';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+
 export const activate = (context: vscode.ExtensionContext) => {
 	const sidebarProvider = new SidebarProvider(context.extensionUri);
 
@@ -12,17 +11,12 @@ export const activate = (context: vscode.ExtensionContext) => {
 		vscode.window.registerWebviewViewProvider("mountainvue-sidebar", sidebarProvider)
   );
 
-	let disposable = vscode.commands.registerCommand('mountainvue.emptyWorkspace', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
+	context.subscriptions.push(vscode.commands.registerCommand('mountainvue.emptyWorkspace', () => {
 		vscode.window.showInformationMessage('Vue project directory not found');
-	});
+		})
+	);
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
 
-	context.subscriptions.push(disposable);
 };
 
 // this method is called when your extension is deactivated
