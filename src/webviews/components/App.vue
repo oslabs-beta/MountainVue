933ce9da5b1eb3dbbeda5e4e74501aeaf7066aa7
Matthew Huang <matthewhuang24@gmail.com>
2021-11-11 13:32:23 -0800

    Upload button function completed; list rendering
    Co-authored-by: Ryan Lim <ryanlim1@users.noreply.github.com>
    Co-authored-by: andytsou19 <andytsou19@users.noreply.github.com>
    Co-authored-by: Sohee <sohee419@users.noreply.github.com>
    Co-authored-by: jo-cella <jo-cella@users.noreply.github.com>
    Co-authored-by: Sean Lee <seanswlee@users.noreply.github.com>

--- src/webviews/components/App.vue
+++ src/webviews/components/App.vue
@@ -1,21 +1,57 @@
 <template>
   <div>
-    <h1>{{ welcome }}</h1>
+    <img :src="logo" />
+    <div class="upload-container">
+      <input
+        class="upload-file"
+        ref="inputFile"
+        type="file"
+        accept="application/JSON"
+        @change="handleUpload"
+      >
+      <button @click="fileUpload()">Upload</button>
+    </div>
+    <tree :tree-data="tree"></tree>
   </div>
 </template>
 
 <script>
-  export default  {
-    name: "App",
-    data() {
-      return {
-        welcome: 'HELLO FROM VUE'
-      }
+import logo from '../img/logo.png';
+import Tree from "./Tree.vue";
+
+export default {
+  name: 'App',
+
+  components: {
+    Tree
+  },
+
+  data() {
+    return {
+      logo,
+      tree: {},
     }
-  };
+  },
+
+  methods: {
+    fileUpload() {
+      const uploadBtn = this.$refs.inputFile;
+      uploadBtn.click();
+    },
+
+    async handleUpload() {
+      const file = this.$refs.inputFile.files[0];
+
+      const response = await file.text();
+      const data = await JSON.parse(response);
+      
+      this.tree = data;
+    },
+  },
+};
 </script>
 
-<style scoped lang="scss">
+<style lang="scss">
   body {
     margin: 0;
   }
@@ -26,10 +62,26 @@
     -moz-osx-font-smoothing: grayscale;
     text-align: center;
     color: #2c3e50;
-    height: 100vh;
+    // height: 100vh;
     display: flex;
     flex-direction: column;
     justify-content: center;
     align-items: center;
+  }
+
+  img {
+    margin: 7px 0;
+  }
+
+  .upload-file {
+    display: none !important;
+  }
+
+  .upload-container {
+    margin-bottom: 20px;
+  }
+
+  button {
+    width: 100%
   }
 </style>
